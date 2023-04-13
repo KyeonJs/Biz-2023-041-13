edca1776e9211255d55846fa23427ed47fc4d207
Kyeonjs <kyeonjs@gmail.com>
2023-04-13 16:41:11 +0900

    workspace

--- hello_01/src/com/callor/hello/HelloYour.java
+++ hello_01/src/com/callor/hello/HelloYour.java
@@ -3,13 +3,11 @@ package com.callor.hello;
 //sourse code,java class file
 
 public class HelloYour {
-	
 	public static void main(String[] args) {
-		System.out.println(30+40);
-		System.out.println(30-40);
-		System.out.println(30*40);
-		System.out.println(30/40);
-		System.out.println(30%40);
+		System.out.println(30 + 40);
+		System.out.println(30 - 40);
+		System.out.println(30 * 40);
+		System.out.println(30 / 40);
+		System.out.println(30 % 40);
 	}
-
 }
---
+++ hello_01/src/com/callor/hello/data/HelloData.java
@@ -0,0 +1,32 @@
+package com.callor.hello.data;
+
+public class HelloData {
+	public static void main(String[] args) {
+		System.out.println("Hello Korea");
+		System.out.println("Hello" + "Korea");
+		System.out.println("Hello     Korea");
+		System.out.println("Hello" +      "Korea");
+		
+		System.out.println(30+40);
+		System.out.println("30" + "40");
+		System.out.println("30 + 40");
+		
+		System.out.println("30 + 40 =" +30 + 40 );
+        //"30+40+30을 연산하려고 시도
+        //이 연산은 문자열 + 숫자 의 연산이다
+        //이럴때는 숫자데이터인 30을 문자열 30으로 바꿔버린다
+		System.out.println("30 + 40 = " + "30" + 40);
+		System.out.println("30 + 40 = 30" + 40);
+		System.out.println("30 + 40 = " + "30" + "40");
+		
+		System.out.println("30 + 40 = " + (30 + 40));
+		System.out.println("30 X 40 = " + 30 * 40);
+//		문자열은 +(연결연산자)만 가능하다
+//		System.out.println("30 - 40 = " + 30 - 40);
+//		System.out.println("30" - "40");
+//		System.out.println("30" * "40");
+//		System.out.println("30" / "40");
+		
+	}
+
+}
